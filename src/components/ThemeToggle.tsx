473c4be34e7184d8dab6732fcd9fc18f4b2ad790
nikhithag61 <nikhithag61@gmail.com>
2025-07-13 17:43:34 +0530

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,7 +5,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@/components/ui/dropdown.tsx";
 import { useTheme } from "next-themes";
 
 export function ThemeToggle() {
